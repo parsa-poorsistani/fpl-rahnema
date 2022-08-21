6ffaf28723acdd44c63d7b03022f7a36d44025c0
Pegah Givehchian <givehchianpegah@yahoo.com>
2022-08-21 14:27:19 +0430

    add default value for models

--- app/models/playerModel.js
+++ app/models/playerModel.js
@@ -2,23 +2,34 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const playerSchema = new Schema({
-  generalId: { type: String, trim: true, required: true, unique: true }, //id
-  fname: { type: String, trim: true, required: true }, //first_name
-  lname: { type: String, trim: true, required: true }, //secondName
-  web_name: { type: String, trim: true, required: true }, //web_name
-  price: { type: String, trim: true, required: true }, //now_cost/10
-  teamId: { type: String, trim: true, required: true }, //team
-  team_code: { type: String, trim: true, required: true }, //team_code
-  photo: { type: String, trim: true, required: true }, //photo
-  positionId: { type: mongoose.Types.ObjectId, trim: true, required: true }, //element_type
-  points: { type: Number, trim: true }, //total_points
-  value_season: { type: Number, trim: true }, //value_season
-  minutes: { type: Number, trim: true }, //minutes
-  goals_scored: { type: Number, trim: true }, //goals_scored
-  yellow_cards: { type: Number, trim: true }, //yellow_cards
-  red_cards: { type: Number, trim: true }, //red_cards
-  influence: { type: Number, trim: true }, //influence
-  creativity: { type: Number, trim: true }, //creativity
-  threat: { type: Number, trim: true }, //threat
-  in_dreamteam: { type: Boolean, trim: true }, //in_dreamteam
+  generalId: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+    default: null,
+  }, //id
+  fname: { type: String, trim: true, required: true, default: null }, //first_name
+  lname: { type: String, trim: true, required: true, default: null }, //secondName
+  web_name: { type: String, trim: true, required: true, default: null }, //web_name
+  price: { type: String, trim: true, required: true, default: null }, //now_cost/10
+  teamId: { type: String, trim: true, required: true, default: null }, //team
+  team_code: { type: String, trim: true, required: true, default: null }, //team_code
+  photo: { type: String, trim: true, required: true, default: null }, //photo
+  positionId: {
+    type: mongoose.Types.ObjectId,
+    trim: true,
+    required: true,
+    default: null,
+  }, //element_type
+  points: { type: Number, trim: true, default: null }, //total_points
+  value_season: { type: Number, trim: true, default: null }, //value_season
+  minutes: { type: Number, trim: true, default: null }, //minutes
+  goals_scored: { type: Number, trim: true, default: null }, //goals_scored
+  yellow_cards: { type: Number, trim: true, default: null }, //yellow_cards
+  red_cards: { type: Number, trim: true, default: null }, //red_cards
+  influence: { type: Number, trim: true, default: null }, //influence
+  creativity: { type: Number, trim: true, default: null }, //creativity
+  threat: { type: Number, trim: true, default: null }, //threat
+  in_dreamteam: { type: Boolean, trim: true, default: False }, //in_dreamteam
 });
--- app/models/positionModel.js
+++ app/models/positionModel.js
@@ -2,15 +2,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const positionSchema = new Schema({
-  generalId: { type: String, trim: true, required: true, unique: true },
-  plural_name: { type: String, trim: true },
-  plural_name_short: { type: String, trim: true },
-  singular_name: { type: String, trim: true },
-  singular_name_short: { type: String, trim: true },
-  squad_count: { type: Number, trim: true },
-  squad_min_play: { type: Number, trim: true },
-  squad_max_play: { type: Number, trim: true },
-  ui_shirt_specific: { type: Number, trim: true },
-  sub_positions_locked: [{ type: Number, trim: true }],
-  element_count: { type: Number, trim: true },
+  generalId: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+    default: null,
+  },
+  plural_name: { type: String, trim: true, default: null },
+  plural_name_short: { type: String, trim: true, default: null },
+  singular_name: { type: String, trim: true, default: null },
+  singular_name_short: { type: String, trim: true, default: null },
+  squad_count: { type: Number, trim: true, default: null },
+  squad_min_play: { type: Number, trim: true, default: null },
+  squad_max_play: { type: Number, trim: true, default: null },
+  ui_shirt_specific: { type: Number, trim: true, default: null },
+  sub_positions_locked: [{ type: Number, trim: true, default: null }],
+  element_count: { type: Number, trim: true, default: null },
 });
