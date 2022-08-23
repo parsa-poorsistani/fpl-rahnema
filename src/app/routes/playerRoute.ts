46afd3225906c53341dc383e3ecf48759d33ee0a
Parsa Poorsistani <poorsistani13@gmail.com>
2022-08-23 13:40:04 +0430

    player find by name added

--- src/app/routes/playerRoute.ts
+++ src/app/routes/playerRoute.ts
@@ -3,6 +3,7 @@ const routes = express.Router();
 import models = require("../../app/models/path");
 import controllers = require("../controllers/path");
 
-routes.get("", controllers.playerController.getPlayers);
+routes.get('/', controllers.playerController.getAllPlayers);
+routes.get('/search',controllers.playerController.getPlayerByName);
 
 module.exports = routes;
