53a4884bb62604e66a868c62c7bc0644734c0bee
Pegah Givehchian <givehchianpegah@yahoo.com>
2022-08-23 15:53:29 +0430

    update positions in db added to mint/ population of position when getting players added/ filter players added

--- src/app/controllers/playerController.ts
+++ src/app/controllers/playerController.ts
@@ -1,8 +1,15 @@
 import models = require("../../app/models/path");
 
 const getPlayers = async (req: any, res: any) => {
-  let players = await models.playerModel.find();
-  models.playerModel.res.status(200).json({ data: players });
+  let players = await models.playerModel
+    .find(req.query.filter ? { positionId: req.query.filter } : null)
+    .populate({
+      path: "position",
+      options: { select: { singular_name_short: 1, generalId: 1 } },
+    })
+    .exec();
+
+  res.status(200).json({ data: players });
 };
 
 module.exports = { getPlayers };
--- src/app/routes/playerRoute.ts
+++ src/app/routes/playerRoute.ts
@@ -2,7 +2,10 @@ const express = require("express");
 const routes = express.Router();
 import models = require("../../app/models/path");
 import controllers = require("../controllers/path");
+import mint = require("../service/mint");
 
+routes.get("/positions", mint.updatePlayerPositionsData);
+routes.get("/playerData", mint.updatePlayerdata);
 routes.get("", controllers.playerController.getPlayers);
 
 module.exports = routes;
