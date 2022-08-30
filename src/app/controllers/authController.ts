8dd3861c8f8f60e902da409d5d6e91ece89c260b
Pegah Givehchian <givehchianpegah@yahoo.com>
2022-08-30 15:29:23 +0430

    add bcrypt to manager model (not tested)

---
+++ src/app/controllers/authController.ts
@@ -0,0 +1,4 @@
+const models = require("../models/path");
+import { Request, Response } from "express";
+
+module.exports = {};
--- src/app/models/managerModel.ts
+++ src/app/models/managerModel.ts
@@ -1,22 +1,23 @@
-import {ObjectId, model, Schema, Types} from 'mongoose';
+import { ObjectId, model, Schema, Types } from "mongoose";
+import bcrypt from "bcrypt";
 
 interface IManager {
-  first_name:String,
-  player_region_name:String,
-  player_region_iso_code_short:String,
-  player_region_iso_code_long:String,
-  last_name:String,
-  password:String,
-  email:String,
-  joined_time:Date,
-  summary_overall_points:Number,
-  summary_overall_rank:Number,
-  summary_event_points:Number,
-  summary_event_rank:Number,
-  current_event:Number,
-  teamName:String,
-  teamId:ObjectId
-};
+  first_name: String;
+  player_region_name: String;
+  player_region_iso_code_short: String;
+  player_region_iso_code_long: String;
+  last_name: String;
+  password: String;
+  email: String;
+  joined_time: Date;
+  summary_overall_points: Number;
+  summary_overall_rank: Number;
+  summary_event_points: Number;
+  summary_event_rank: Number;
+  current_event: Number;
+  teamName: String;
+  teamId: ObjectId;
+}
 
 const managerSchema = new Schema<IManager>({
   first_name: {
@@ -29,17 +30,17 @@ const managerSchema = new Schema<IManager>({
     required: true,
     maxlength: 20,
   },
-  player_region_name:{
-    type:String,
-    required:true
+  player_region_name: {
+    type: String,
+    required: true,
   },
-  player_region_iso_code_long:{
-      type:String,
-      required:true
+  player_region_iso_code_long: {
+    type: String,
+    required: true,
   },
-  player_region_iso_code_short:{
-      type:String,
-      required:true
+  player_region_iso_code_short: {
+    type: String,
+    required: true,
   },
   joined_time: {
     type: Date,
@@ -69,10 +70,28 @@ const managerSchema = new Schema<IManager>({
     type: String,
     required: true,
   },
-  teamId :{
+  teamId: {
     type: Types.ObjectId,
-    ref:'Team'
+    ref: "Team",
   },
+});
+
+managerSchema.pre("save", function (this: any, next: any) {
+  if (this.isModified("password")) {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(this.password, salt, (error, hash) => {
+        if (error) {
+          return next(error);
+        }
+        this.password = hash;
+        return next(null, this);
+      });
+    });
+  }
+  return next(null, this);
 });
 
 const Manager = model("Manager", managerSchema);
