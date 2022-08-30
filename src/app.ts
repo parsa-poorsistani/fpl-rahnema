78d78fced24b4e6efdb624f6a27f2be4f8e92f7f
Pegah Givehchian <givehchianpegah@yahoo.com>
2022-08-30 09:25:34 +0430

    bugfix

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const db = require('./app/service/db-connection');
+const db = require("./app/service/db-connection");
 const mint = require("./app/service/mint");
 const routes = require("./app/routes/path");
 const cronJob = require("cron").CronJob;
@@ -13,7 +13,7 @@ app.use("/api/v1/manager", routes.managerRoute);
 app.use("/api/v1/player", routes.playerRoute);
 app.use("/api/v1/event", routes.eventRoute);
 
-const setupServer = async() => {
+const setupServer = async () => {
   await db();
   app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
@@ -21,7 +21,6 @@ const setupServer = async() => {
 };
 
 setupServer();
-mint.connectDb();
 
 // let setCronJob = (): any => {
 //   let job = new cronJob("0 0 * * 0", () => {
