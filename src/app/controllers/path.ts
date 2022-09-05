19f8289d97f9b63fadea09bb658122e06d33431d
Pegah Givehchian <givehchianpegah@yahoo.com>
2022-09-05 17:01:12 +0430

    add path for auth

--- src/app/controllers/path.ts
+++ src/app/controllers/path.ts
@@ -2,6 +2,14 @@ const playerController = require("./playerController");
 const userController = require("./userController");
 const eventController = require("./eventController");
 const managerController = require("./managerController");
-const teamController = require('./teamController');
+const teamController = require("./teamController");
+const authController = require("./authController");
 
-export { playerController, userController, eventController, managerController, teamController };
+export {
+  playerController,
+  userController,
+  eventController,
+  managerController,
+  teamController,
+  authController,
+};
