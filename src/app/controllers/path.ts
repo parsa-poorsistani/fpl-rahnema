2e70a6d5cc426411023d02a16a9404160b2a13e1
Parsa Poorsistani <poorsistani13@gmail.com>
2022-09-06 10:23:46 +0430

    security leak fixed and package.json

--- src/app/controllers/path.ts
+++ src/app/controllers/path.ts
@@ -1,13 +1,11 @@
 const playerController = require("./playerController");
-const userController = require("./userController");
 const eventController = require("./eventController");
-const managerController = require("./userController");
+const managerController = require("./managerController");
 const authController = require("./authController");
 const teamController = require("./teamController");
 
 export {
   playerController,
-  userController,
   eventController,
   managerController,
   authController,
