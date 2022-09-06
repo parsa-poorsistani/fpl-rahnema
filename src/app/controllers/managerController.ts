73a32907e230c004cfb34c33aa25fbdf33390fdb
Parsa Poorsistani <poorsistani13@gmail.com>
2022-09-06 16:27:22 +0430

    manager controller cleaned

--- src/app/controllers/managerController.ts
+++ src/app/controllers/managerController.ts
@@ -1,24 +1,6 @@
 const models = require("../models/path");
 import { Request, Response } from "express";
 
-const createManager = async (req: Request, res: Response) => {
-  try {
-    const team = await models.teamModel.create();
-    req.body.teamId = team._id;
-    const manager = await models.managerModel.create(req.body);
-
-    res.status(200).json({ manager });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
-
-const getManager = (req: Request, res: Response) => {
-  const managerId = req.params.id;
-  const manager = models.managerModel.findById(managerId);
-  return res.status(200).json({ manager });
-};
-
 const getDashboard = async (req: Request, res: Response) => {
   let manager = await models.managerModel
     .findById(req._id)
@@ -27,7 +9,5 @@ const getDashboard = async (req: Request, res: Response) => {
 };
 
 module.exports = {
-  createManager,
-  getManager,
   getDashboard,
 };
