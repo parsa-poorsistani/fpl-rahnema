fe394c2610f2f061729543afecbdd6335f6dcaac
Pegah Givehchian <givehchianpegah@yahoo.com>
2022-09-10 10:29:24 +0430

    manager controller refactor

--- src/app/controllers/managerController.ts
+++ src/app/controllers/managerController.ts
@@ -1,11 +1,16 @@
-const models = require("../models/path");
+import models = require("../models/path");
 import { Request, Response } from "express";
+import { GlobalError } from "../helpers/error/globalError";
 
 const getDashboard = async (req: Request, res: Response) => {
-  let manager = await models.managerModel
-    .findById(req._id)
-    .populate(["teamId", { path: "teamId", populate: "picks.player" }]);
-  return res.status(200).json({ data: manager });
+  try {
+    let manager = await models.managerModel
+      .findById(req._id)
+      .populate(["teamId", { path: "teamId", populate: "picks.player" }]);
+    return res.status(200).json({ data: manager });
+  } catch (err) {
+    res.status(403).json({ msg: err });
+  }
 };
 
 module.exports = {
