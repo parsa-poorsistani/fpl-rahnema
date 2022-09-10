3421b0537cd611077b9667c3c427dafb73402f1c
Pegah Givehchian <givehchianpegah@yahoo.com>
2022-09-10 15:28:58 +0430

    refactor commit 2

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const db = require("./app/service/db-connection");
+const db = require("./app/database/db-connection");
 const mint = require("./app/service/mint");
 const routes = require("./app/routes/path");
 const cronJob = require("cron").CronJob;
-const service = require('./app/service/service'); 
+const service = require("./app/service/service");
 const port: Number = parseInt(<string>process.env.APPLICATION_PORT);
 const morgan = require("morgan");
 const cors = require("cors");
@@ -14,14 +14,13 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 
-
 app.use("/api/v1/auth", routes.authRoute);
 app.use("/api/v1/managers", routes.managerRoute);
 app.use("/api/v1/players", routes.playerRoute);
 app.use("/api/v1/teams", routes.teamRoute);
 app.use("/api/v1/events", routes.eventRoute);
 
-const setupServer = async () => {  
+const setupServer = async () => {
   await db();
   app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
