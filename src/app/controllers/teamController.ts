054eb3efe73a07fd7a826f9df5a110d94b084f2b
Parsa Poorsistani <poorsistani13@gmail.com>
2022-09-12 10:48:31 +0430

    delete player bug fixed

--- src/app/controllers/teamController.ts
+++ src/app/controllers/teamController.ts
@@ -54,9 +54,8 @@ const deletePlayerFromTeam = async (req: Request, res: Response) => {
     const currentBudget: number = manager.budget;
     const playerId: number = req.body.id;
     const index: number = req.body.index;
-    const player = await models.playerModel.findOne(playerId);
+    const player = await models.playerModel.findById(playerId);
     let team = manager.teamId.picks;
-    let place: number = 0;
 
     if (team === null) {
       res.status(404).json({ msg: "manager not found" });
