3afaa93fdf1a5339c3f32bc42d0cc07b5893bad3
Parsa Poorsistani <poorsistani13@gmail.com>
2022-09-12 10:26:03 +0430

    nb bug fixed

--- src/app/controllers/managerController.ts
+++ src/app/controllers/managerController.ts
@@ -11,7 +11,7 @@ const getDashboard = async (req: Request, res: Response) => {
     manager.budget = Math.round(manager.budget*10)/10;
     manager.save();
     
-    return res.status(200).json({ data: {manager,15-nb});
+    return res.status(200).json({ data: {manager,nb}});
   } catch (err) {
     res.status(403).json({ msg: err });
   }
